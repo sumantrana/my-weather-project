7cb179c1380b300fd32ecc274648bf7cfeddbb26
Pradeep Loganathan <loganathan.pradeep@gmail.com>
2022-09-19 19:08:28 +1000

    Update WeatherForecastController.cs

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace AccuWeather.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Raining", "Snowing"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Raining", "Snowing", "Humid"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
